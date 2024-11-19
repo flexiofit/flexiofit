3066bc37fbd73450e7009bf71a2f73c6cf225731
kitparl <pranshu.java@gmail.com>
2024-11-19 10:41:53 +0530

    adding logger {02}

--- backend/internal/logging/logger.go
+++ backend/internal/logging/logger.go
@@ -1,14 +1,26 @@
 // internal/logging/logger.go
-// internal/logging/logger.go
 package logging
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+// ANSI color codes
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+	colorBlue   = "\033[34m"
+	colorPurple = "\033[35m"
+	colorCyan   = "\033[36m"
+	colorGray   = "\033[37m"
 )
 
 type LoggerConfig struct {
@@ -21,23 +33,58 @@ type LoggerConfig struct {
 	Development bool
 }
 
-// CreateCustomEncoder creates a more detailed and readable encoder
-func createCustomEncoder(config zapcore.EncoderConfig) zapcore.Encoder {
-	config.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	config.EncodeTime = customTimeEncoder
-	config.EncodeDuration = zapcore.StringDurationEncoder
-	config.EncodeCaller = zapcore.FullCallerEncoder
-	return zapcore.NewConsoleEncoder(config)
+// customTimeEncoder formats timestamp with millisecond precision and green color
+func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	timeStr := fmt.Sprintf("%s%s%s",
+		colorGreen, // Changed from colorGray to colorGreen
+		t.Format("2006-01-02 15:04:05.000 MST"),
+		colorReset,
+	)
+	enc.AppendString(timeStr)
 }
 
-// customTimeEncoder formats timestamp with millisecond precision
-func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString(t.Format("2006-01-02 15:04:05.000 MST"))
+// customLevelEncoder adds colors to log levels
+func customLevelEncoder(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	var levelColor string
+	switch l {
+	case zapcore.DebugLevel:
+		levelColor = colorPurple
+	case zapcore.InfoLevel:
+		levelColor = colorBlue
+	case zapcore.WarnLevel:
+		levelColor = colorYellow
+	case zapcore.ErrorLevel:
+		levelColor = colorRed
+	default:
+		levelColor = colorReset
+	}
+	enc.AppendString(fmt.Sprintf("%s%s%s", levelColor, l.String(), colorReset))
+}
+
+// customCallerEncoder formats the caller path with color
+func customCallerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(fmt.Sprintf("%s%s%s", colorCyan, caller.TrimmedPath(), colorReset))
+}
+
+// createCustomEncoder creates a more detailed and readable encoder with colors
+func createCustomEncoder(config zapcore.EncoderConfig) zapcore.Encoder {
+	config.EncodeLevel = customLevelEncoder
+	config.EncodeTime = customTimeEncoder
+	config.EncodeCaller = customCallerEncoder
+	config.EncodeDuration = zapcore.StringDurationEncoder
+	config.MessageKey = "message"
+	config.LevelKey = "level"
+	config.TimeKey = "timestamp"
+	config.CallerKey = "caller"
+	config.NameKey = "logger"
+	config.StacktraceKey = "stacktrace"
+	config.LineEnding = zapcore.DefaultLineEnding
+
+	return zapcore.NewConsoleEncoder(config)
 }
 
 // NewLogger creates a highly configurable zap logger
 func NewLogger(config LoggerConfig) (*zap.Logger, error) {
-	// Determine log level
 	var level zapcore.Level
 	switch config.Level {
 	case "debug":
@@ -52,18 +99,6 @@ func NewLogger(config LoggerConfig) (*zap.Logger, error) {
 		level = zap.InfoLevel
 	}
 
-	// Encoder configuration
-	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:       "timestamp",
-		LevelKey:      "level",
-		NameKey:       "logger",
-		CallerKey:     "caller",
-		MessageKey:    "message",
-		StacktraceKey: "stacktrace",
-		LineEnding:    zapcore.DefaultLineEnding,
-	}
-
-	// Create log rotation
 	var logWriter zapcore.WriteSyncer
 	if config.Filepath != "" {
 		logRotator := &lumberjack.Logger{
@@ -78,20 +113,17 @@ func NewLogger(config LoggerConfig) (*zap.Logger, error) {
 		logWriter = zapcore.AddSync(os.Stdout)
 	}
 
-	// Create core with multiple outputs
 	core := zapcore.NewCore(
-		createCustomEncoder(encoderConfig),
+		createCustomEncoder(zapcore.EncoderConfig{}),
 		zapcore.NewMultiWriteSyncer(logWriter, zapcore.AddSync(os.Stdout)),
 		zap.NewAtomicLevelAt(level),
 	)
 
-	// Logger options
 	opts := []zap.Option{
 		zap.AddCaller(),
 		zap.AddStacktrace(zap.ErrorLevel),
 	}
 
-	// Development mode
 	if config.Development {
 		opts = append(opts, zap.Development())
 	}
@@ -99,13 +131,11 @@ func NewLogger(config LoggerConfig) (*zap.Logger, error) {
 	return zap.New(core, opts...), nil
 }
 
-// Global logger wrapper with enhanced functionality
 var (
 	Log   *zap.Logger
 	Sugar *zap.SugaredLogger
 )
 
-// InitializeLogger sets up the global logger
 func InitializeLogger(config LoggerConfig) error {
 	var err error
 	Log, err = NewLogger(config)
@@ -116,7 +146,6 @@ func InitializeLogger(config LoggerConfig) error {
 	return nil
 }
 
-// Sync flushes any buffered log entries
 func Sync() error {
 	if err := Log.Sync(); err != nil {
 		return err
@@ -124,7 +153,6 @@ func Sync() error {
 	return Sugar.Sync()
 }
 
-// WithFields creates a contextual logger with additional fields
 func WithFields(fields ...zap.Field) *zap.Logger {
 	return Log.With(fields...)
 }
