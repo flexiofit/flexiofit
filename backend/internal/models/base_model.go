8da41de4e44e8e7a7ef3f530c6294d7d99e0d67a
kitparl <pranshu.java@gmail.com>
2024-12-05 12:33:56 +0530

    update model

--- backend/internal/models/base_model.go
+++ backend/internal/models/base_model.go
@@ -6,8 +6,8 @@ import (
 )
 
 type BaseModel struct {
-	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
+	ID        uint           `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `json:"column:created_at"`
+	UpdatedAt time.Time      `json:"column:updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 }
--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -6,16 +6,16 @@ import (
 
 type User struct {
 	BaseModel
-	FirstName    string `gorm:"size:100;not null"`        
-	MiddleName   string `gorm:"size:100"`                 
-	LastName     string `gorm:"size:100;not null"`        
-	Email        string `gorm:"size:100;unique;not null"` 
-	Username     string `gorm:"size:100;unique"` 
-	Mobile       string `gorm:"size:20; unique; not null"`
-	UserType     USERROLE `gorm:"not null;default:-1"`
-	PasswordHash string `gorm:"not null"`
-	CreatedBy    int `gorm:"size:50"`
-	UpdatedBy    int `gorm:"size:50"`
+	FirstName    string `gorm:"column:first_name;size:25;not null"`        
+	MiddleName   string `gorm:"column:middle_name;size:25"`                 
+	LastName     string `gorm:"column: last_name;size:25;not null"`        
+	Email        string `gorm:"column: email;size:100;unique;not null"` 
+	Username     string `gorm:"column: username;size:100;unique"` 
+	Mobile       string `gorm:"column:mobile;size:20; unique; not null"`
+	UserType     USERROLE `gorm:"column: user_type;not null;default:-1"`
+	PasswordHash string `gorm:"column:password_hash;not null"`
+	CreatedBy    int `gorm:"column:created_by;size:50"`
+	UpdatedBy    int `gorm:"column:updated_by;size:50"`
 
 	// GymPartner   GymPartner `gorm:"foreignkey:GymPartnerID"` // This refers to the GymPartner model, and GORM will use the field name `GymPartnerID` in the User table
 	GymPartner []GymPartner `gorm:"foreignKey:UserID"`
